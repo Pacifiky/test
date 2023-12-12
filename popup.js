48870ca1c7640b07327dd594a7223eb1f8130827
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-12-12 07:57:02 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
-function popup(){
-    alert(1);
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "start"});
-  });
-
-button1=document.getElementById("button1");
-button1.addEventListener('click', popup)
+function popup() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"message": "start"});
+    });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("button1").addEventListener("click", popup);
+});
