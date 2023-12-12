1b928af2813e3bb6d61fa2754f3b1aec98c0dd7d
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-12-12 07:54:17 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,77 +1,10 @@
-const fileInput = document.getElementById('inputfile');
-let title = false;
-let listname = "";
+function popup(){
+    alert(1);
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "start"});
+  });
 
-fileInput.addEventListener('change', (event) => {
-    const files = event.target.files;
-    const fr=new FileReader();
-    fr.onload = () =>{
-      let data = fr.result;
-      let arr = data.split('\n');
-      listname = arr[0];
-      document.getElementById('listname').value = listname;
-      document.getElementById('bstarti').addEventListener("click",()=>startImport(arr),false)
-    }
-    fr.readAsText(files[0]);
-});
-
-function grey(id){
-    document.getElementById(id).classList.add("disabled");
+button1=document.getElementById("button1");
+button1.addEventListener('click', popup)
 }
-
-function ungrey(id){
-    document.getElementById(id).classList.remove("disabled");
-}
-
-async function sendMessageToActiveTab(message) {
-  const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-  const response = await chrome.tabs.sendMessage(tab.id, message);
-  // do something with response here, not outside the function
-  console.log(response);
-}
-
-function startImport(arr){
-    for(let i = 1;i<arr.length;i++){
-        sendMessageToActiveTab({
-            type: "nav",
-            data: {
-                url: nav[i],
-            }
-        });
-        sendMessageToActiveTab({
-            type: "import",
-            data: {
-                name: listname,
-                arr[i+1] ? next: arr[i+1] : next: null,
-            }
-        });
-    } 
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('bsdown').addEventListener("click",(e)=>{
-        sendMessageToActiveTab({
-            type: "scrolldown",
-            data: {
-            
-            }
-        });
-    },false)
-    
-    document.getElementById('beplay').addEventListener("click",(e)=>{
-        sendMessageToActiveTab({
-            type: "exportplay",
-            data: {
-                title: title,
-            }
-        });
-    },false)
-    
-    document.getElementById('cexti').addEventListener("change",(e)=>{
-        title = !title;
-    },false)
-    
-    document.getElementById('listname').addEventListener("change",(e)=>{
-        listname = document.getElementById('listname').value;
-    },false)
-})
