7faceac7091a38b793a5f1429b47b8d0a272c221
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-12-17 20:30:29 -0700

    Update content-script.js

--- content-script.js
+++ content-script.js
@@ -1,13 +1,15 @@
 chrome.runtime.onMessage.addListener(
     function(obj, sender, res) {
         console.log(obj.message);
-        if( obj.message === "start" ) {
+        if(obj.message === "start") {
             start();
+        }
+        if(obj.message === "service-worker") {
+            chrome.runtime.sendMessage("tab");
         }
     }
 );
 
 function start(){
     alert("started");
-    chrome.runtime.sendMessage("tab");
 };
