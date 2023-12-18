dd2d98539b015262b5fa40fe3ece7153b83b3e3d
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-12-17 20:20:10 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,21 +5,9 @@ async function sendMessageToActiveTab(message) {
   console.log(response);
 }
 
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if(tab.url && (tab.url.includes("youtube.com/watch"))){
-    chrome.tabs.sendMessage(tabId, {
-      type: "video",
-      data: {
-        url: tab.url,
-    },
-    });
-  }
-  else if(tab.url && (tab.url.includes("youtube.com/playlist"))){
-    chrome.tabs.sendMessage(tabId, {
-      type: "playlist",
-      data: {
-        url: tab.url,
-    },
-    });
-  }
-})
+chrome.runtime.onMessage.addListener((obj, sender, res)=>{
+  if(obj.message == "hi"){
+    chrome.runtime.sendMessage({message: "service-worker"});
+    sendMessageToActiveTab({message: "service-worker"});
+  };
+});
